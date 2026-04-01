0d867c6eba1089881d6d1029f2a84b4911506e24
X <xking012344@gmail.com>
2026-04-01 22:11:55 +0000

    starting point....

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
 //rust final
 //CHECKERS
 
+use std::str;
+
 fn main() {
-    println!("Hello, world!");
+    println!("GUESS WHAT... WE'RE PLAYING CHECKERS (rust edition....)");
+
+    
+}
+
+const FRAME: &str = "├───┼───┼───┼───┼───┼───┼───┼───┼";
+const ROW_TEMPLATE : &str = "│ A │ B │ C │ D │ E │ F │ G │ H │";
+
+enum TokenColor{
+    Red,
+    Black,
+}
+
+enum TokenState{
+    Normal,
+    King,
+}
+
+struct Token{
+    color: TokenColor,
+    state: TokenState,
+}
+
+type Board = [Option<Token>; 64];
+
+fn renderRow(board: &Board, row: usize) -> String{
+    format!("{}", ROW_TEMPLATE
+        .replace("A", )
+        .replace("B", )
+        .replace("C", )
+        .replace("D", )
+        .replace("E", )
+        .replace("F", )
+        .replace("G", )
+        .replace("H", ))
+        //.replace("A", getTokenChar(board[8 * row + 0]))
+        //getTokenChar????
+}
+
+fn renderBoard(board: &Board){
+    let mut output = String::new();
+    output += FRAME + "\n";
+
+    for row in 0..8{
+        output += renderRow(board) + "\n" + FRAME + "\n"
+    }
+
+    println!("{output}");
 }
