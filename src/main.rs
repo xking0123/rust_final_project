6e8e12e896fb03f17795232b85ce36a8a0c2ed29
X <xking012344@gmail.com>
2026-04-08 21:27:08 +0000

    progress?

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 //rust final
 //CHECKERS
 
-use std::str;
+use std::{iter::zip, str};
 
 fn main() {
     println!("GUESS WHAT... WE'RE PLAYING CHECKERS (rust edition....)");
 
+    //try making starting board/rendering it
     
+    //try to get moving pieces (validation later)
 }
 
 const FRAME: &str = "├───┼───┼───┼───┼───┼───┼───┼───┼";
 const ROW_TEMPLATE : &str = "│ A │ B │ C │ D │ E │ F │ G │ H │";
+const ROW_TARGETS: [&str; 8] = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
 enum TokenColor{
     Red,
@@ -27,29 +30,38 @@ struct Token{
     state: TokenState,
 }
 
-type Board = [Option<Token>; 64];
-
-fn renderRow(board: &Board, row: usize) -> String{
-    format!("{}", ROW_TEMPLATE
-        .replace("A", )
-        .replace("B", )
-        .replace("C", )
-        .replace("D", )
-        .replace("E", )
-        .replace("F", )
-        .replace("G", )
-        .replace("H", ))
-        //.replace("A", getTokenChar(board[8 * row + 0]))
-        //getTokenChar????
+impl Token {
+    fn to_string(&self) -> String {
+        todo!()
+    }
 }
 
-fn renderBoard(board: &Board){
+type Board = [Option<Token>; 64];
+
+fn render_Row(board: &Board, row: usize) -> String {
+    //
+    let row = &board[(row * 8)..(8 + row * 8)];
+    let tokens = row.iter().map(|space| match space {
+        Some(token) => token.to_string(),
+        None => " ".to_string(),
+    });
+
+    //making template for 1 row string so that strings (board spaces? can be replaced with tokens)
+    let mut row = ROW_TEMPLATE.to_string();
+    for (target, token) in ROW_TARGETS.iter().zip(tokens) {
+        row = row.replace(target, &token);
+    }
+    
+    row
+}
+
+fn render_Board(board: &Board){
     let mut output = String::new();
-    output += FRAME + "\n";
+    output += FRAME;
+    output += "\n";
 
     for row in 0..8{
-        output += renderRow(board) + "\n" + FRAME + "\n"
+        output += format!("{}\n{FRAME}\n", render_Row(board, row)).as_str();
     }
-
     println!("{output}");
 }
